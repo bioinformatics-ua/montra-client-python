efb45f0c5ac19971f59931dd75324670bc20ec03
Pedro Freire <p7freire@gmail.com>
2019-06-04 10:23:49 +0100

    token authentication

--- montra.py
+++ montra.py
@@ -18,12 +18,32 @@ from requests.auth import HTTPDigestAuth
 
 URL = 'https://bioinformatics.ua.pt/ehden'
 COMM = "ehden"
+AUTH_TYPE = 'basic'
+ERROR_MESSAGES = {
+    "bad_auth_type": "Authentication type not valid. Authentication type should be set as 'basic' or 'token' ",
+    "bad_auth_params": ("Authentication parameters are missing! Please provide valid credentials." 
+                        "You can use either an username and password for a basic authentication or a valid API token using token authentication"),
+    "bad_get_request_generic": "Something wrong with the request!"
+}
 
 class Montra:
-    def __init__(self, url=URL):
-        self.ENDPOINT = url
+    def __init__(self, url=URL, auth_type=AUTH_TYPE, username=None, password=None, token=None):
 
-    def search_datasets(self, user, password, questionnaire=COMM):
+        #params validation
+        if auth_type not in ['basic', 'token']:
+            raise ValueError(ERROR_MESSAGES["bad_auth_type"])
+
+        if ( auth_type == 'basic' and (username is None or password is None) ) \
+            and ( auth_type == 'token' and token is None ):
+            raise ValueError(ERROR_MESSAGES["bad_auth_params"])
+
+        self.ENDPOINT = url
+        self.USERNAME = username
+        self.PASSWORD = password
+        self.TOKEN = token
+        self.AUTH_TYPE = auth_type
+
+    def search_datasets(self, questionnaire=COMM):
         """
         Search for the questionnaires in all communities
 
@@ -31,9 +51,9 @@ class Montra:
         """
         url = self.ENDPOINT + "/api/questionnaires/?search=" + str(questionnaire)
 
-        return self.__get_request(url=url, auth=(user, password))
+        return self.__get_request(url=url)
 
-    def get_dataset(self, user, password, questionnaireSlug=COMM):
+    def get_dataset(self, questionnaireSlug=COMM):
         """
         Gets the dataset by the slug, which is the identifier
 
@@ -41,15 +61,15 @@ class Montra:
         """
         url = self.ENDPOINT + "/api/questionnaires/" + str(questionnaireSlug)
         
-        return self.__get_request(url=url, auth=(user, password))
+        return self.__get_request(url=url)
 
-    def get_dataentry(self, user, password, **args):
+    def get_dataentry(self, **args):
         if(len(args) == 1):
-            return self.__get_dataentry_by_hash(user, password, args["fingerprintHash"])
+            return self.__get_dataentry_by_hash(args["fingerprintHash"])
         else:
-            return self.__get_dataentry_by_acronym(user, password, args["acronym"], args["questionnaireSlug"])
+            return self.__get_dataentry_by_acronym(args["acronym"], args["questionnaireSlug"])
 
-    def __get_dataentry_by_hash(self, user, password, fingerprintHash):
+    def __get_dataentry_by_hash(self, fingerprintHash):
         """
         Gets the fingerprint by the fingprint hash
 
@@ -57,9 +77,9 @@ class Montra:
         """
         url = self.ENDPOINT + "/api/fingerprints/" + str(fingerprintHash)
         
-        return self.__get_request(url=url, auth=(user, password))
+        return self.__get_request(url=url)
 
-    def __get_dataentry_by_acronym(self, user, password, acronym, questionnaireSlug=COMM):
+    def __get_dataentry_by_acronym(self, acronym, questionnaireSlug=COMM):
         """
         Gets the fingerprint by the fingprint acronym and the questionnaire slug
 
@@ -67,9 +87,9 @@ class Montra:
         """
         url = self.ENDPOINT + "/api/fingerprint-cslug-fslug/" + str(questionnaireSlug) + "/" + str(acronym)
 
-        return self.__get_request(url=url, auth=(user, password))
+        return self.__get_request(url=url)
 
-    def list_answer(self, user, password, fingerprintHash):
+    def list_answer(self, fingerprintHash):
         """
         Gets the list of available questions to get or update data of the fingerprint hash
 
@@ -77,9 +97,9 @@ class Montra:
         """
         url = self.ENDPOINT + "/api/fingerprints/" + str(fingerprintHash) + "/answers"
 
-        return self.__get_request(url=url, auth=(user, password))
+        return self.__get_request(url=url)
 
-    def get_answer(self, user, password, fingerprintHash, question):
+    def get_answer(self, fingerprintHash, question):
         """
         Gets the the question of the fingerprint hash
 
@@ -87,22 +107,40 @@ class Montra:
         """
         url = self.ENDPOINT + "/api/fingerprints/" + str(fingerprintHash) + "/answers/" + str(question)
 
-        return self.__get_request(url=url, auth=(user, password))
+        return self.__get_request(url=url)
 
-    def post_answer(self, user, password, fingerprintHash, question, newAnswer):
+    def post_answer(self, fingerprintHash, question, newAnswer):
         """
         Post a new answer in the question of the fingerprint hash
 
         return: json with question and answer
         """
-        url = self.ENDPOINT + "/api/fingerprints/" + str(fingerprintHash) + "/answers/" + str(question) + "/"
-
-        response = requests.put(url, auth=(user, password), data={"data":newAnswer})
-        return response.json()
-
-    def __get_request(self, url, auth):
         try:
-            response = requests.get(url, auth=auth)
+            url = self.ENDPOINT + "/api/fingerprints/" + str(fingerprintHash) + "/answers/" + str(question) + "/"
+
+            if self.AUTH_TYPE == 'basic':
+                response = requests.put(url, auth=(self.USERNAME, self.PASSWORD), data={"data":newAnswer})
+            elif self.AUTH_TYPE == 'token':
+                response = requests.put(url, headers={'Authorization': 'Token ' + self.TOKEN}, data={"data":newAnswer})
+
+            response.raise_for_status()
+
             return response.json()
-        except:
-            raise Exception("Something wrong with the request!")
+        except requests.exceptions.HTTPError as err:
+            print err
+            sys.exit(1)
+
+    def __get_request(self, url):
+        try:
+            if self.AUTH_TYPE == 'basic':
+                response = requests.get(url, auth=(self.USERNAME, self.PASSWORD))
+            elif self.AUTH_TYPE == 'token':
+                response = requests.get(url, headers={'Authorization': 'Token ' + self.TOKEN})
+            else:
+                raise ValueError(ERROR_MESSAGES["bad_auth_type"])
+
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.HTTPError as err:
+            print err
+            sys.exit(1)
--- tests.py
+++ tests.py
@@ -1,21 +1,43 @@
+import unittest
 from montra import Montra
+
+# class TestAuthenticationMethods(unittest.TestCase):
+
+#     def test_basic_auth(self):
+#         self.assertEqual('foo'.upper(), 'FOO')
+
+#     def test_isupper(self):
+#         self.assertTrue('FOO'.isupper())
+#         self.assertFalse('Foo'.isupper())
+
+#     def test_split(self):
+#         s = 'hello world'
+#         self.assertEqual(s.split(), ['hello', 'world'])
+#         # check that s.split fails when the separator is not a string
+#         with self.assertRaises(TypeError):
+#             s.split(2)
+
+# if __name__ == '__main__':
+#     unittest.main()
 
 
 #Used to test locally during development
 user="admin"
-password="12345"
+password="123456"
+token="de4611bcf0c6e393404fac095dab09fad01c1554s"
 
-montra = Montra(url="http://127.0.0.1:8000")
+#montra = Montra(url="http://127.0.0.1:8000", username=user, password=password)
+montra = Montra(url="http://127.0.0.1:8000", auth_type='token', token=token)
 
 print " -- search_datasets --"
-listOfQuestionnaires = montra.search_datasets(user, password, questionnaire="Demo Observational")
+listOfQuestionnaires = montra.search_datasets(questionnaire="Demo Observational")
 questionnaireSlug = listOfQuestionnaires[0]["slug"]
 print questionnaireSlug
 print " -- search_datasets done -- \n"
 
 
 print " -- get_dataset --"
-questionnaire = montra.get_dataset(user, password, questionnaireSlug=questionnaireSlug)
+questionnaire = montra.get_dataset( questionnaireSlug=questionnaireSlug)
 print questionnaire
 fingerprintHash = questionnaire["fingerprint_set"][0]
 print fingerprintHash
@@ -23,31 +45,31 @@ print " -- search_datasets done -- \n"
 
 
 print " -- get_dataentry 1 param--"
-fingerprintOp1 = montra.get_dataentry(user, password, fingerprintHash=fingerprintHash)
+fingerprintOp1 = montra.get_dataentry(fingerprintHash=fingerprintHash)
 print fingerprintOp1
 print " -- get_dataentry 1 param done -- \n"
 
 
 print " -- get_dataentry 2 param --"
-fingerprintOp2 = montra.get_dataentry(user, password, acronym="ADC", questionnaireSlug="adcohort")
+fingerprintOp2 = montra.get_dataentry(acronym="ADC", questionnaireSlug="adcohort")
 print fingerprintOp2
 fingerprintHash2 = fingerprintOp2["fingerprint_hash"]
 print fingerprintHash2
 print " -- get_dataentry 2 param done -- \n"
 
 print " -- list_answer --"
-answers = montra.list_answer(user, password, fingerprintHash=fingerprintHash2)
+answers = montra.list_answer(fingerprintHash=fingerprintHash2)
 print answers
 print " -- list_answer done --"
 
 print " -- get_answer --"
 question = answers[0]["question"]
 print question
-answer = montra.get_answer(user, password, fingerprintHash=fingerprintHash2, question=question)
+answer = montra.get_answer(fingerprintHash=fingerprintHash2, question=question)
 print answer
 print " -- get_answer done--"
 
 print " -- post_answer --"
-newAnswer = montra.post_answer(user, password, fingerprintHash=fingerprintHash2, question=question, newAnswer="newAnswer")
+newAnswer = montra.post_answer(fingerprintHash=fingerprintHash2, question=question, newAnswer="newAnswer")
 print newAnswer
 print " -- post_answer done--"
