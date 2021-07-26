49de84742fefe868c17b5aa90f6f5158e32280e0
Tiberio Baptista <tiberio.baptista@ua.pt>
2021-07-26 17:48:35 +0100

    Updated print functions to python3 syntax

--- tests.py
+++ tests.py
@@ -7,47 +7,45 @@ password="12345"
 
 montra = Montra(url="http://127.0.0.1:8000")
 
-print " -- search_datasets --"
+print(" -- search_datasets --")
 listOfQuestionnaires = montra.search_datasets(user, password, questionnaire="Demo Observational")
 questionnaireSlug = listOfQuestionnaires[0]["slug"]
-print questionnaireSlug
-print " -- search_datasets done -- \n"
+print(questionnaireSlug)
+print(" -- search_datasets done -- \n")
 
 
-print " -- get_dataset --"
+print(" -- get_dataset --")
 questionnaire = montra.get_dataset(user, password, questionnaireSlug=questionnaireSlug)
-print questionnaire
-fingerprintHash = questionnaire["fingerprint_set"][0]
-print fingerprintHash
-print " -- search_datasets done -- \n"
+print(questionnaire)
+print(fingerprint)
+print(" -- search_datasets done -- \n")
 
 
-print " -- get_dataentry 1 param--"
-fingerprintOp1 = montra.get_dataentry(user, password, fingerprintHash=fingerprintHash)
-print fingerprintOp1
-print " -- get_dataentry 1 param done -- \n"
+print(" -- get_dataentry 1 param--")
+print(fingerprintOp1)
+print(" -- get_dataentry 1 param done -- \n")
 
 
-print " -- get_dataentry 2 param --"
+print(" -- get_dataentry 2 param --")
 fingerprintOp2 = montra.get_dataentry(user, password, acronym="ADC", questionnaireSlug="adcohort")
-print fingerprintOp2
+print(fingerprintOp2)
 fingerprintHash2 = fingerprintOp2["fingerprint_hash"]
-print fingerprintHash2
-print " -- get_dataentry 2 param done -- \n"
+print(fingerprintHash2)
+print(" -- get_dataentry 2 param done -- \n")
 
-print " -- list_answer --"
+print(" -- list_answer --")
 answers = montra.list_answer(user, password, fingerprintHash=fingerprintHash2)
-print answers
-print " -- list_answer done --"
+print(answers)
+print(" -- list_answer done --")
 
-print " -- get_answer --"
+print(" -- get_answer --")
 question = answers[0]["question"]
-print question
+print(question)
 answer = montra.get_answer(user, password, fingerprintHash=fingerprintHash2, question=question)
-print answer
-print " -- get_answer done--"
+print(answer)
+print(" -- get_answer done--")
 
-print " -- post_answer --"
+print(" -- post_answer --")
 newAnswer = montra.post_answer(user, password, fingerprintHash=fingerprintHash2, question=question, newAnswer="newAnswer")
-print newAnswer
-print " -- post_answer done--"
+print(newAnswer)
+print(" -- post_answer done--")
